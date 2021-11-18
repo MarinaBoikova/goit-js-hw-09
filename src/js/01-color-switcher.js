d31bc31149c543ff35d2858d91148a6674b45a5b
Marina Boikova <boymar84@gmail.com>
2021-11-18 23:06:35 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const btnStartRef = document.querySelector('[data-start]');
+  const btnStopRef = document.querySelector('[data-stop]');
+  let  colorId;
+
+  btnStartRef.addEventListener('click', () => {
+     colorId = setInterval(() => document.querySelector('body').style.backgroundColor = getRandomHexColor(), 1000);
+    btnStopRef.disabled = false;
+    btnStartRef.disabled = true;
+});
+
+btnStopRef.addEventListener('click', () => {
+    clearInterval(colorId);
+    btnStopRef.disabled = true;
+    btnStartRef.disabled = false;
+});
